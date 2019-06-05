10b1351acc53838e1064d05963055c1825f85289
Luc Boussant-roux <luc.boussant-roux@sigmalis.ch>
2019-06-05 21:47:08 +0200

    Show my first pokemon

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -5,6 +5,8 @@ import Style from './Home.style';
 class Home extends React.Component {
   render(): React.ReactNode {
     const pokemon = 'Carapuce';
+    const carapuceId = 7;
+    const imageUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + carapuceId + ".png";
 
     return (
       <Style.Intro>
@@ -13,7 +15,9 @@ class Home extends React.Component {
           Tu vas pouvoir apprendre tout ce qu'il faut sur React, Redux et Symfony, et attraper des
           pokemons !
         </div>
-        <div>Commence par créer ton premier pokemon: {pokemon}</div>
+        <div>Commence par créer ton premier pokemon:</div>
+        <div><img src={imageUrl} /></div>
+        <div>{carapuceId}: {pokemon}</div>
       </Style.Intro>
     );
   }
