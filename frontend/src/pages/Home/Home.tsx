907159d490cb31130ae030d11ad4c0f63addc89d
Luc Boussant-roux <luc.boussant-roux@sigmalis.ch>
2019-06-09 14:51:48 +0200

    Add a loader styled

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -2,7 +2,8 @@ import * as React from 'react';
 
 import Pokemon from 'components/Pokemon';
 import { FormattedMessage } from 'react-intl';
-import { StyledList, Title } from './Home.style';
+import { ReactComponent as Loader } from '../../assets/loader.svg';
+import { StyledList, StyledLoader, Title } from './Home.style';
 
 import { makeGetRequest } from 'services/networking/request';
 
@@ -16,33 +17,38 @@ interface PokemonInterface {
 interface Props { }
 interface State {
   pokemons: PokemonInterface[];
+  loading: boolean;
 }
 
 class Home extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
-    this.state = { pokemons: [] };
+    this.state = { pokemons: [], loading: true };
   }
 
   async componentDidMount() {
     const pokemonDict = await makeGetRequest('/pokemon');
     const pokemons = JSON.parse(pokemonDict.text);
-    this.setState({ pokemons });
+    this.setState({ pokemons, loading: false });
   }
 
   render(): React.ReactNode {
-    const { pokemons } = this.state;
+    const { pokemons, loading } = this.state;
 
     return (
       <React.Fragment>
         <Title>
           <FormattedMessage id="home.welcome-message" />
         </Title>
-        <StyledList>
-          {pokemons && pokemons.map(pokemon => (
-            <Pokemon name={pokemon.name} id={pokemon.id} weight={pokemon.weight} height={pokemon.height} key={pokemon.id} />
-          ))}
-        </StyledList>
+        {loading ?
+          (<StyledLoader>
+            <Loader />
+          </StyledLoader>) :
+          (<StyledList>
+            {pokemons.map(pokemon => (
+              <Pokemon name={pokemon.name} id={pokemon.id} weight={pokemon.weight} height={pokemon.height} key={pokemon.id} />
+            ))}
+          </StyledList>)}
       </React.Fragment>
     );
   }
