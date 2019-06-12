f460c91e3cfc1b688e29cf57b976811fc6ab974f
Luc Boussant-roux <luc.boussant-roux@sigmalis.ch>
2019-06-12 22:31:30 +0200

    Change to functional composant

--- frontend/src/components/Pokemon/Pokemon.tsx
+++ frontend/src/components/Pokemon/Pokemon.tsx
@@ -1,32 +1,27 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import { FormattedMessage } from 'react-intl';
 
 import { Body, Card, Image, Title } from './Pokemon.style';
 
-interface IProps {
+interface Props {
   name: string,
   id: number,
   weight: number,
   height: number,
 };
 
-class Pokemon extends PureComponent<IProps> {
-  render(): React.ReactNode {
-    const { name, id, weight, height } = this.props;
-    const imageUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + id + ".png";
+const getImageUrl = (id: number) => ("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + id + ".png");
 
-    return (
-      <Card>
-        <Title>{name}</Title>
-        <Image src={imageUrl} />
-        <Body>
-        <FormattedMessage id="pokemon.id" values={{ id }} />
-        <FormattedMessage id="pokemon.weight" values={{ weight }} />
-        <FormattedMessage id="pokemon.height" values={{ height }} />
-        </Body>
-      </Card>
-    );
-  }
-}
+const Pokemon: React.FunctionComponent<Props> = (props) => (
+  <Card>
+    <Title>{props.name}</Title>
+    <Image src={getImageUrl(props.id)} />
+    <Body>
+      <FormattedMessage id="pokemon.id" values={{ id: props.id }} />
+      <FormattedMessage id="pokemon.weight" values={{ weight: props.weight }} />
+      <FormattedMessage id="pokemon.height" values={{ height: props.height }} />
+    </Body>
+  </Card>
+);
 
 export default Pokemon;
