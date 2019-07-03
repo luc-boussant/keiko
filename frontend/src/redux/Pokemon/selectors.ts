b0d6707d0c93fd0ad7f1dacc4d6db73c835f5822
Luc Boussant-roux <luc.boussant-roux@sigmalis.ch>
2019-07-03 22:58:02 +0200

    Add a selector for the new field in state

--- frontend/src/redux/Pokemon/selectors.ts
+++ frontend/src/redux/Pokemon/selectors.ts
@@ -2,3 +2,4 @@ import { RootState } from 'redux/types';
 import { PokemonType } from './reducer';
 
 export const getPokemons = (state: RootState): PokemonType[] => Object.values(state.pokemon);
+export const getPokemon = (state: RootState): PokemonType => state.pokemon.selectedPokemon;
